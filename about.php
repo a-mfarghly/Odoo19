8c247b28deeb26e21593f425e3fe5c3740f956c9
amr-mahmoud <mamr32278@gmail.com>
2026-02-03 10:25:20 +0200

    rfdgfdgdf

---
+++ about.php
@@ -0,0 +1,18 @@
+
+<?php
+
+// about.php
+include 'header.php';
+
+echo "<h1>About Us</h1>";
+
+include 'footer.php';
+?>
+<?php
+// about.php
+include 'header.php';
+
+echo "<h1>About Us</h1>";
+
+include 'footer.php';
+?>
